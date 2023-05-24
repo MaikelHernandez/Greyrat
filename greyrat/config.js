cdfe83e089039c7cc2cf77d42932de6fdb8b88ef
MaikelHernandez <124740844+MaikelHernandez@users.noreply.github.com>
2023-05-24 15:58:57 -0400

    config.js

--- greyrat/config.js
+++ greyrat/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const env = process.env;
 
 class Auth {
-    // Make sure to add all details in '.env' file
+    
     static TOKEN = env.TOKEN;
     static CLIENT_ID = env.CLIENT_ID;
 
